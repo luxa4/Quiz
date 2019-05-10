c3422b5196fe745589a5225d444d6c18862d6692
Alexandr <luxa4ru@gmail.com>
2019-05-10 14:00:43 +0300

    change GUI Player

--- QuizCardPlayer.java
+++ QuizCardPlayer.java
@@ -1,2 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
 public class QuizCardPlayer {
+
+    JFrame frame;
+    JPanel mainPanel;
+    JButton button;
+    List<QuizCard> cardList;
+    JTextArea display;
+
+
+
+
+    public static void main(String[] args) {
+        QuizCardPlayer quizCardPlayer = new QuizCardPlayer();
+        quizCardPlayer.go();
+    }
+
+    public void go() {
+        frame = new JFrame("Quiz Player");
+
+
+        mainPanel = new JPanel();
+
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem Load = new JMenuItem("Open...");
+
+        menu.add(Load);
+        menuBar.add(menu);
+
+        display = new JTextArea(6,20);
+        Font font = new Font("sanserif", Font.ITALIC,24);
+        display.setFont(font);
+
+        button = new JButton("Show Question");
+
+        mainPanel.add(menuBar);
+        mainPanel.add(display);
+        mainPanel.add(button);
+
+
+        frame.getContentPane().add(BorderLayout.CENTER,mainPanel);
+
+        frame.setSize(500,600);
+        frame.setVisible(true);
+    }
 }
